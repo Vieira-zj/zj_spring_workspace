7544394f7456de98cfeb127cea01eff785275a62
zhengjin <zivieira@163.com>
2020-05-02 12:37:22 +0800

    Add springboot config demos.

--- zj_springboot_project/run.sh
+++ zj_springboot_project/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 
-mvn spring-boot:run
+mvn spring-boot:run -Dmaven.test.skip=true
 echo "run spring boot done."
--- zj_springboot_project/src/main/java/com/zhengjin/springboot/Ch522Application.java
+++ zj_springboot_project/src/main/java/com/zhengjin/springboot/Ch522Application.java
@@ -1,12 +1,21 @@
 package com.zhengjin.springboot;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.concurrent.TimeUnit;
 
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.ErrorPage;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,7 +23,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.zhengjin.spring_boot_starter_hello.HelloService;
 import com.zhengjin.springboot.ch6_2_3.config.AuthorSettings;
-import com.zhengjin.springboot.ch7_2_4.bean.Person;
 
 @Controller
 @SpringBootApplication
@@ -32,8 +40,9 @@ public class Ch522Application {
 	@Autowired
 	HelloService helloService;
 
+	// path: /helloboot
 	@RequestMapping("/")
-	public @ResponseBody String index() {
+	public @ResponseBody String defaultMsg() {
 		String ret = "Hello Spring Boot";
 		ret += "<br>" + String.format("book author:%s, name:%s", bookAuthor, bookName);
 		ret += "<br>" + String.format("author name:%s, age:%d", authorSettings.getName(), authorSettings.getAge());
@@ -41,27 +50,70 @@ public class Ch522Application {
 		return ret;
 	}
 
-	// path: /helloboot/thymeleaf
-	@RequestMapping("/thymeleaf")
-	public String people(Model model) {
-		Person single = new Person("aa", 11);
-
-		List<Person> people = new ArrayList<Person>();
-		Person p1 = new Person("xx", 11);
-		Person p2 = new Person("yy", 22);
-		Person p3 = new Person("zz", 33);
-		people.add(p1);
-		people.add(p2);
-		people.add(p3);
-
-		model.addAttribute("singlePerson", single);
-		model.addAttribute("people", people);
-		return "thymeleaf";
+	// path: /helloboot/index
+	@RequestMapping("/index")
+	public String index(Model model) {
+		model.addAttribute("bodyContent", "Spring Boot Index Page");
+		return "index";
 	}
 
 	public static void main(String[] args) {
 
 		SpringApplication.run(Ch522Application.class, args);
+	}
+
+	// 配置tomcat servlet, 使用EmbeddedServletContainerFactory代替
+//	@Component
+//	public static class CustomServletContainer implements EmbeddedServletContainerCustomizer {
+//
+//		@Override
+//		public void customize(ConfigurableEmbeddedServletContainer container) {
+//			container.setSessionTimeout(10, TimeUnit.MINUTES);
+//		}
+//	}
+
+	// 配置tomcat servlet
+	@Bean
+	@Profile("dev")
+	public EmbeddedServletContainerFactory devServletContainer() {
+		TomcatEmbeddedServletContainerFactory factory = new TomcatEmbeddedServletContainerFactory();
+		factory.setSessionTimeout(10, TimeUnit.MINUTES);
+		factory.addErrorPages(new ErrorPage(HttpStatus.NOT_FOUND, "/404.html"));
+		return factory;
+	}
+
+	@Bean
+	@Profile("prod")
+	public EmbeddedServletContainerFactory prodServletContainer() {
+		TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory() {
+			@Override
+			protected void postProcessContext(Context context) {
+				SecurityConstraint securityConstraint = new SecurityConstraint();
+				securityConstraint.setUserConstraint("CONFIDENTIAL");
+
+				SecurityCollection collection = new SecurityCollection();
+				collection.addPattern("/*");
+				securityConstraint.addCollection(collection);
+				context.addConstraint(securityConstraint);
+			}
+		};
+
+		tomcat.setSessionTimeout(10, TimeUnit.MINUTES);
+		tomcat.addErrorPages(new ErrorPage(HttpStatus.NOT_FOUND, "/404.html"));
+		tomcat.addAdditionalTomcatConnectors(httpConnector());
+		return tomcat;
+	}
+
+	// 配置http转向https
+	@Bean
+	@Profile("prod")
+	public Connector httpConnector() {
+		Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+		connector.setScheme("http");
+		connector.setPort(8081);
+		connector.setSecure(false);
+		connector.setRedirectPort(8443);
+		return connector;
 	}
 
 }
